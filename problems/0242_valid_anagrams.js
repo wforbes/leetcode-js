d715773a453e94c608ccfe422ee1c8c68be0c73c
Will Forbes <wforbes87@gmail.com>
2025-10-02 01:00:06 -0700

    Update 0242_valid_anagrams.js
    add alternate solutions

--- problems/0242_valid_anagrams.js
+++ problems/0242_valid_anagrams.js
@@ -14,3 +14,28 @@ export const validAnagram = (s, t) => {
 		return count === 0;
 	});
 }
+
+export const isAnagram = function (s, t) {
+	if (s.length !== t.length) {
+		return false;
+	}
+
+	const countS = {};
+	const countT = {};
+	for (let i = 0; i < s.length; i++) {
+		countS[s[i]] = (countS[s[i]] || 0) + 1;
+		countT[t[i]] = (countT[t[i]] || 0) + 1;
+	}
+
+	for (const key in countS) {
+		if (countS[key] !== countT[key]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+export const isAnagram2 = function (s, t) {
+	if (s.length !== t.length) return false;
+	return s.split("").sort().join("") === t.split("").sort().join("");
+};
