ba4a28bb50792f2037c22d6950544043d9948bd6
Will Forbes <wforbes87@gmail.com>
2025-10-02 11:57:46 -0700

    add two pointer solution for #1 Two Sum

--- problems/0001_two_sum.js
+++ problems/0001_two_sum.js
@@ -4,12 +4,30 @@
  * @return {number[]}
  */
 export function twoSum(nums, target) {
-    const m = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        const diff = target - nums[i];
-        if (m.has(diff)) {
-            return [m.get(diff), i]
-        }
-        m.set(nums[i], i);
-    }
+	const m = new Map();
+	for (let i = 0; i < nums.length; i++) {
+		const diff = target - nums[i];
+		if (m.has(diff)) {
+			return [m.get(diff), i]
+		}
+		m.set(nums[i], i);
+	}
+}
+
+export function twoSum2(arr, target) {
+	arr.sort((a, b) => a - b);
+
+	let left = 0;
+	let right = arr.length - 1;
+	while (left < right) {
+		let sum = arr[left] + arr[right];
+		if (sum === target) {
+			return [arr[left], arr[right]];
+		} else if (sum < target) {
+			left++;
+		} else {
+			right--;
+		}
+	}
+	return [];
 }
