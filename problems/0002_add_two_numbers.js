22d9b2e6b593133f9bd44124bb36e5721b1c833a
Will Forbes <wforbes87@gmail.com>
2025-10-13 09:38:46 -0700

    add improved version for #2 Add Two Numbers

--- problems/0002_add_two_numbers.js
+++ problems/0002_add_two_numbers.js
@@ -43,3 +43,29 @@ export function addTwoNumbers(l1, l2) {
     }
     return solution;
 };
+
+export function addTwoNumbers2(l1, l2) {
+	let newNode = new ListNode(0);
+	let head = newNode;
+	let sum = 0;
+	let carry = 0;
+	while (l1 !== null || l2 !== null || sum > 0) {
+		if (l1 !== null) {
+			sum += l1.val;
+			l1 = l1.next;
+		}
+		if (l2 !== null) {
+			sum += l2.val;
+			l2 = l2.next;
+		}
+		if (sum > 9) {
+			carry = 1;
+			sum = sum - 10;
+		}
+		head.next = new ListNode(sum);
+		head = head.next;
+		sum = carry;
+		carry = 0;
+	}
+	return newNode.next;
+}
--- problems/0002_add_two_numbers.test.js
+++ problems/0002_add_two_numbers.test.js
@@ -1,10 +1,22 @@
 import { test, expect } from "vitest";
-import { addTwoNumbers, ListNode } from "./0002_add_two_numbers";
+import { addTwoNumbers, ListNode, addTwoNumbers2 } from "./0002_add_two_numbers";
 
 test("leetcode example", () => {
 	const l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
 	const l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
 	let result = addTwoNumbers(l1, l2);
+	const resultArray = [];
+	while (result) {
+		resultArray.push(result.val);
+		result = result.next;
+	}
+	expect(resultArray).toEqual([7, 0, 8]);
+});
+
+test("v2: leetcode example", () => {
+	const l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+	const l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+	let result = addTwoNumbers2(l1, l2);
 	const resultArray = [];
 	while (result) {
 		resultArray.push(result.val);
